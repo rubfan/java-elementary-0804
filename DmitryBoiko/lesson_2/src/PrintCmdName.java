686027e80fa61f01a4b230bd60b088ce298fb083
dmytro-boiko-dev <dmytroboiko1@gmail.com>
2020-04-14 19:12:21 +0300

    Dmitry Boiko: print args app

---
+++ DmitryBoiko/lesson_2/src/PrintCmdName.java
@@ -0,0 +1,9 @@
+class PrintCmdName {
+	public static void main (String[] args){
+		
+		System.out.print("Hello ");
+		for (String arg : args) {
+            System.out.print(arg + " ");
+        }
+	}
+}
