67af430e7e1529ec2e4528abb4aba724e3c68e22
Oleksandr Baz <telefon.baz@gmail.com>
2020-04-26 00:25:03 +0300

    HomeWork4

---
+++ AlexBaz/Lesson_4/src/Paragraph4.java
@@ -0,0 +1,8 @@
+public class Paragraph4 {
+    public static void main(String[] args) {
+        int var1 = Integer.parseInt(args[0]);
+        int var2 = Integer.parseInt(args[1]);
+        int hypotenuse2 = var1*var1 + var2*var2;
+        System.out.println(Math.sqrt(hypotenuse2));
+    }
+}
