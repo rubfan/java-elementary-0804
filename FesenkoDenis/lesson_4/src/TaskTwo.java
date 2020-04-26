b84d712755b1c2e1c6399eeeb3a522e95c124cb6
Your Name <you@example.com>
2020-04-26 22:10:54 +0300

    Changes in second task

--- FesenkoDenis/lesson_4/src/TaskTwo.java
+++ FesenkoDenis/lesson_4/src/TaskTwo.java
@@ -1,10 +1,10 @@
 public class TaskTwo {
 
     public static void main(String[] params) {
-        double first = Double.parseDouble(params[0]);
-        double second = Double.parseDouble(params[1]);
-        double third = Double.parseDouble(params[2]);
-        double msg = (first >= second & first >= third) & !(first < third || first < second) ? first
+        int first = Integer.parseInt(params[0]);
+        int second = Integer.parseInt(params[1]);
+        int third = Integer.parseInt(params[2]);
+        int msg = (first >= second & first >= third) & (first > third || first > second) ? first
                 : second >= third ? second
                 : third;
         System.out.println(msg);
