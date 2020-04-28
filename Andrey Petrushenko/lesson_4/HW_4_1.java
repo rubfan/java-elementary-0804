11fd193a71d758395a79afc9eb98125aae40bfc1
Andrey <panama8432@gmail.com>
2020-04-28 13:54:29 +0300

    homework_4

---
+++ Andrey Petrushenko/lesson_4/HW_4_1.java
@@ -0,0 +1,8 @@
+public class HW_4_1 {
+    public static void main (String[] args){
+        float a=Float.parseFloat (args[0]);
+        float b=Float.parseFloat (args[1]);
+        float sum=a+b;
+        System.out.println("Сума = " +sum );
+    }
+}
