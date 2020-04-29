53cb901bde5d7fabb53190c8c318eacc18895227
Andrey <panama8432@gmail.com>
2020-04-29 23:47:08 +0300

    homework_5

---
+++ Andrey Petrushenko/lesson_5/Hw_5_4.java
@@ -0,0 +1,23 @@
+public class Hw_5_4 {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        if (a <= b && a <= c) {
+            System.out.println("min number a = " + a);
+        }
+        else{}
+            if (b <= a || b <= c) {
+                System.out.println("min number b = " + b);
+            }
+            else{}
+                if (c <= a && c <= b) {
+                    System.out.println("min number c = " + c);
+                }
+                else{}
+            }
+        }
+
+
+
+
