f6e3ac6162b43b69f5638576046659bae2d13356
DBCH <dbch@ukr.net>
2020-05-03 12:15:28 +0300

    Homework Lesson 6

--- DenysChevychelov/lesson_6/HomeworkLesson6.java
+++ DenysChevychelov/lesson_6/HomeworkLesson6.java
@@ -1,5 +1,5 @@
 package com.company;
-public class HomeworkLesson5 {
+public class HomeworkLesson6 {
     public static void main(String[] args) {
 //          1) Создайте программу, выводящую на экран все четырёхзначные
 //          числа последовательности 1000 1003 1006 1009 1012 1015 ….
