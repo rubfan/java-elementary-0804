2ed0a7ffcffd9af141598c1750e370ae94504644
Oleksandr Baz <telefon.baz@gmail.com>
2020-05-07 12:53:47 +0300

    Lamp_java_files

---
+++ AlexBaz/Lesson_7/Shockproof.java
@@ -0,0 +1,9 @@
+public class Shockproof {
+    public void valueLamp(int i) {
+        if (i >= 200) {
+            System.out.println("Lamp is shockproof");
+        } else {
+            System.out.println("Lamp is not shockproof, choose item with higher price");
+        }
+    }
+}
