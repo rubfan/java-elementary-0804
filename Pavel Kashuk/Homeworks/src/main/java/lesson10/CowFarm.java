206a782e50101782d67cc47d3a948d5da0301a57
Pavel Kashuk <pavel.kashuk@gmail.com>
2020-05-28 01:45:36 +0300

    add homework 10

---
+++ Pavel Kashuk/Homeworks/src/main/java/lesson10/CowFarm.java
@@ -0,0 +1,23 @@
+package lesson10;
+
+public class CowFarm {
+
+    private int amountOfCow;
+    private double farmPrice;
+
+    public void setFarmPrice(double farmPrice) {
+        this.farmPrice = farmPrice;
+    }
+
+    public double getFarmPrice() {
+        return farmPrice;
+    }
+
+    public void setAmountOfCow(int amountOfCow) {
+        this.amountOfCow = amountOfCow;
+    }
+
+    public int getAmountOfCow() {
+        return amountOfCow;
+    }
+}
