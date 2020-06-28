628bc2826b0038a009c556ef749b92cdf12fe681
Helen Sotnik <politovaoo@gmail.com>
2020-06-28 17:24:59 +0300

    Change in HW13

--- HelenSotnik/lesson_13/src/Basket.java
+++ HelenSotnik/lesson_13/src/Basket.java
@@ -9,11 +9,11 @@ public class Basket {
         this.productsInBasket = products;
     }
 
-    public Product[] getProducts() {
+    protected Product[] getProducts() {
         return productsInBasket;
     }
 
-    public void setProducts(Product[] basketPr) {
+    protected void setProducts(Product[] basketPr) {
         this.productsInBasket = basketPr;
     }
 
